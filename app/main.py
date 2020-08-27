5d5ad41aa62224e221fe746376ead99ed013fcf8
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-27 20:41:15 +0430

    getTheJoke completed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 """DevJoke is a cli program that returns programming jokes randomly. (Internet required)"""
 
+import sys
+import json
 import requests
-import property
+import properties
 
 class DevJoke:
     """DevJoke main class - DevJoke program starts from this class."""
@@ -12,7 +14,18 @@ class DevJoke:
 
     def getTheJoke(self):
         """This method sends GET request to API_URL defined in property.py"""
-        pass
+
+        try:
+
+            response = requests.get(properties.API_URL).text
+            data = json.loads(response)
+
+            self.joke = data["joke"]
+
+        except:
+
+            print("Can't connect to the server baby!! Go and complete your project, dead-line comes soon!")
+            sys.exit(1)
 
 if __name__ == "__main__":
     app = DevJoke()
