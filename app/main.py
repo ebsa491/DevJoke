ffc243d7d0a40d53ed350b6db04dcd60500442e1
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-27 20:03:02 +0430

    DevJoke init method doc string added

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import requests
 class DevJove:
     """DevJoke main class - DevJoke program starts from this class."""
     def __init__(self):
+        """INIT"""
         pass
 
 if __name__ == "__main__":
