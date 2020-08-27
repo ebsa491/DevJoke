acbe0bccd48c43227b08e0465ec0a37d4401b5e7
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-27 20:23:41 +0430

    getTheJoke method added

--- app/main.py
+++ app/main.py
@@ -4,9 +4,13 @@ import requests
 
 class DevJoke:
     """DevJoke main class - DevJoke program starts from this class."""
-    
+
     def __init__(self):
         """INIT"""
+        pass
+
+    def getTheJoke(self):
+        """This method sends GET request to API_URL defined in property.py"""
         pass
 
 if __name__ == "__main__":
