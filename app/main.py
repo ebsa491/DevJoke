34a183411ca6da42a35521d881df4810bd383695
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-27 20:18:24 +0430

    Sorting doc strings

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 """DevJoke is a cli program that returns programming jokes randomly. (Internet required)"""
+
 import requests
 
 class DevJoke:
     """DevJoke main class - DevJoke program starts from this class."""
+    
     def __init__(self):
         """INIT"""
         pass
--- app/property.py
+++ app/property.py
@@ -1,2 +1,3 @@
 """DevJoke program static values."""
+
 API_URL = "https://geek-jokes.sameerkumar.website/api?format=json"
